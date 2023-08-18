297decfa9179dfcfb819dcd619e9a2961deedb67
Abhishek <you@example.com>
2023-08-18 22:07:07 +0530

    dev-stage9

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import './Navbar.module.css'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
--- frontend/src/modules/Pages/MainPage/MainPage.jsx
+++ frontend/src/modules/Pages/MainPage/MainPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './MainPage.module.css'
+import Navbar from '../../../components/Navbar/Navbar'
 
 const MainPage = () => {
   return (
-    <div>MainPage he y</div>
+    <div>
+      <Navbar/>
+    </div>
   )
 }
 
