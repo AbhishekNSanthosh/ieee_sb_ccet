ea960842accbb85711ef9e07c4ef3d3e00f2ca34
Abhishek <you@example.com>
2023-08-19 20:10:26 +0530

    day2-dev_stage6

--- frontend/src/modules/Pages/MainPage/MainPage.jsx
+++ frontend/src/modules/Pages/MainPage/MainPage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import styles from './MainPage.module.css'
 import Navbar from '../../components/Navbar/Navbar'
 import Banner from '../../components/Banner/Banner'
+import About from '../../components/About/About'
 
 const MainPage = () => {
   return (
     <div className={styles.home}>
       <Navbar/>
       <Banner/>
+      <About/>
     </div>
   )
 }
