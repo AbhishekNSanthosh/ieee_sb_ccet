d202886d557bec503f525500633b7b24aa0da60f
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2023-11-15 19:08:59 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,33 @@
-import {
-  RouterProvider,
-  createBrowserRouter,
-  Navigate
-} from "react-router-dom";
-import { lazy } from "react";
-import './App.module.css'
-import RightClickPrevention from "./AddOn/RightClickPrevention";
-
-const MainPage = lazy(() =>
-  import('./modules/Pages/MainPage/MainPage')
-)
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainPage />
-    }
-  ]);
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <RightClickPrevention /> */} 
-      {/* enable after completion */}
-      <RouterProvider router={router} />
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
--- frontend/src/services/ieeeData.js
+++
@@ -1,6 +0,0 @@
-export const navData = [
-    { item: "About Us", link: 'about-section', anyLink: true },
-    { item: "Execom", link: '', anyLink: false },
-    { item: "Events", link: '', anyLink: false },
-    { item: "Updates", link: '', anyLink: false },
-];
